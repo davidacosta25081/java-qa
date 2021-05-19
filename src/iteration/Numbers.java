2ae836849fc464792107a68e46d21348be5b6b82
David <davidacosta25081@yahoo.com>
2021-05-19 09:15:08 -0500

    AD: Adding functionality to numberToWord function for numbers between 1-19.

--- src/iteration/Numbers.java
+++ src/iteration/Numbers.java
@@ -7,40 +7,47 @@ public class Numbers {
 	public static void main(String[] args) {
 
 		addingDigits();
-		numberToWord(34);
+		numberToWord(10);
 	}
 
 	public static void addingDigits() {
-	  Scanner scan = new Scanner(System.in);
-	  int num;
-	  do {
-	    System.out.println("Please enter a two digit number between 10 and 99");
-	    num = scan.nextInt();
-	    } while(num < 10 || num > 99);
-	  int x = num % 10;
-      num = num / 10;
-      int y = x + num; 
-      System.out.println("The added digits of number typed are = " + y + "! \n");
-    }
-
-	
-	public static void numberToWord(int num) {
-
-	  String[] multOfTen = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-	  String[] oneToNine = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-	  int secondNum = num % 10;
-	  num = num / 10;
-	  String first = multOfTen[num];
-	  String second = oneToNine[secondNum];
-	  System.out.println("Number to word = " + first + "-" + second +"!");
+		Scanner scan = new Scanner(System.in);
+		int num;
+		do {
+			System.out.println("Please enter a two digit number between 10 and 99 \n");
+			num = scan.nextInt();
+		} while (num < 10 || num > 99);
+		int x = num % 10;
+		num = num / 10;
+		int y = x + num;
+		System.out.println("\nThe added digits of number typed are = " + y + "! \n");
 	}
 
+	
+	
+	public static void numberToWord(int num) {
+        if (num < 20) {
+        String[] oneToNineteen = {"","One","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};	
+        System.out.println("Number to word = " + oneToNineteen[num] + "!");	
+        
+        }else {
+		  String[] multOfTen = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+		  String[] oneToNine = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		  int secondNum = num % 10;
+		  num = num / 10;
+		  String first = multOfTen[num];
+		  String second = oneToNine[secondNum];
+		  System.out.println("Number to word = " + first + "-" + second + "!");
+		}
+    }
 }
 
+	
 /*
-   Create a method that takes a number 10-99 and adds the two digits together
-   for example 74 = 7 + 4 = 11.
- 
-   Create a method that when given the number 1-99 returns a String representation of this number, 
-   for example 1 = one, 11 = eleven, 21 = twenty-one.
-*/
+ * Create a method that takes a number 10-99 and adds the two digits together
+ * for example 74 = 7 + 4 = 11.
+ * 
+ * Create a method that when given the number 1-99 returns a String
+ * representation of this number, for example 1 = one, 11 = eleven, 21 =
+ * twenty-one.
+ */
